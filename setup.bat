bb9cbd9351919dcff9817956f5fe16aef6d3801d
Faayakhun <faayakhun@gmail.com>
2021-07-13 15:24:32 +0700

    fixing setup bug

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.gi" /ve /t REG_SZ /d "E:\gamelauncher\manifest.json" /f
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.hi" /ve /t REG_SZ /d "E:\gamelauncher\manifest.json" /f
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.p4" /ve /t REG_SZ /d "E:\gamelauncher\manifest.json" /f
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.ha" /ve /t REG_SZ /d "E:\gamelauncher\manifest.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.gi" /ve /t REG_SZ /d "E:\gamelauncher\native-apps\genshin.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.hi" /ve /t REG_SZ /d "E:\gamelauncher\native-apps\honkai.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.p4" /ve /t REG_SZ /d "E:\gamelauncher\native-apps\persona.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.gamelauncher.ha" /ve /t REG_SZ /d "E:\gamelauncher\native-apps\hades.json" /f
